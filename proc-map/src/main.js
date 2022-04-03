a3ad13635f5ed0859d49f036d77c0533be175889
Gustas <gustass1@gmail.com>
2022-04-03 14:53:36 +0200

    Proc-map: Added trees

--- proc-map/src/main.js
+++ proc-map/src/main.js
@@ -189,6 +189,10 @@ function makeHex(height, position) {
 		}
 	} else if (height > DIRT_HEIGHT) {
 		dirtGeo = mergeBufferGeometries([geo, dirtGeo]);
+
+		if (Math.random() > 0.8) {
+			grassGeo = mergeBufferGeometries([grassGeo, tree(height, position)]);
+		}
 	} else if (height > GRASS_HEIGHT) {
 		grassGeo = mergeBufferGeometries([geo, grassGeo]);
 	} else if (height > SAND_HEIGHT) {
@@ -227,4 +231,19 @@ function stone(height, position) {
 	geo.translate(position.x + px, height, position.y + pz);
 
 	return geo;
+}
+
+function tree(height, position) {
+	const treeHeight = Math.random() * 1 + 1.25;
+
+	const geo = new CylinderGeometry(0, 1.5, treeHeight, 3);
+	geo.translate(position.x, height + treeHeight * 0 + 1, position.y);
+
+	const geo2 = new CylinderGeometry(0, 1.15, treeHeight, 3);
+	geo2.translate(position.x, height + treeHeight * 0.6 + 1, position.y);
+
+	const geo3 = new CylinderGeometry(0, 0.8, treeHeight, 3);
+	geo3.translate(position.x, height + treeHeight * 1.25 + 1, position.y);
+
+	return mergeBufferGeometries([geo, geo2, geo3]);
 }
