f8442f9e5c99c8c8d9ae09e49b039483a9f28602
Gustas <gustass1@gmail.com>
2022-04-25 00:09:39 +0200

    Test-proc: Updated map generation

--- test-proc/src/main.ts
+++ test-proc/src/main.ts
@@ -15,17 +15,17 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.outputEncoding = THREE.sRGBEncoding;
-renderer.physicallyCorrectLights = true;
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+// renderer.toneMapping = THREE.ACESFilmicToneMapping;
+// renderer.outputEncoding = THREE.sRGBEncoding;
+// renderer.physicallyCorrectLights = true;
+// renderer.shadowMap.enabled = true;
+// renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-camera.position.x = 30;
-camera.position.y = 10;
-camera.position.z = 30;
+camera.position.x = 100;
+camera.position.y = 80;
+camera.position.z = 100;
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -41,33 +41,42 @@ animate();
 
 // Map code
 
-const base = { height: 0, color: new THREE.Color(0x331802) };
-const water = { height: 0.2, color: new THREE.Color(0x5a78e6) };
-const sand = { height: 0.3, color: new THREE.Color(0xe3c866) };
-const grass = { height: 0.5, color: new THREE.Color(0x6ce036) };
-const dirt = { height: 0.7, color: new THREE.Color(0x804c1f) };
-const stone = { height: 0.8, color: new THREE.Color(0xa8a8a8) };
+const water1 = { height: 0, color: new THREE.Color(0x5a78e6) };
+const water2 = { height: 0.1, color: new THREE.Color(0x5a78e6) };
+const water3 = { height: 0.2, color: new THREE.Color(0x5a78e6) };
+const sand1 = { height: 0.25, color: new THREE.Color(0xef5e342) };
+const sand2 = { height: 0.3, color: new THREE.Color(0xef5bf42) };
+const sand3 = { height: 0.35, color: new THREE.Color(0xef5a442) };
+const grass1 = { height: 0.4, color: new THREE.Color(0x6ce036) };
+const grass2 = { height: 0.6, color: new THREE.Color(0x27c918) };
+const grass3 = { height: 0.8, color: new THREE.Color(0x148709) };
 
-for (let i = 0; i < 10; i++) {
-	for (let j = 0; j < 10; j++) {
-		const height = Math.abs(simplex.noise2D(i, j));
+for (let i = 0; i < 100; i++) {
+	for (let j = 0; j < 100; j++) {
+		const height = (simplex.noise2D(i * 0.02, j * 0.02) + 1) * 0.5;
 		const geometry = new THREE.BoxGeometry(1, 1, 1);
 		geometry.translate(i, 0, j);
 		console.log(height);
 		const material = new THREE.MeshBasicMaterial();
 
-		if (height > stone.height) {
-			material.color = stone.color;
-		} else if (height > dirt.height) {
-			material.color = dirt.color;
-		} else if (height > grass.height) {
-			material.color = grass.color;
-		} else if (height > sand.height) {
-			material.color = sand.color;
-		} else if (height > water.height) {
-			material.color = water.color;
-		} else if (height > base.height) {
-			material.color = base.color;
+		if (height > grass3.height) {
+			material.color = grass3.color;
+		} else if (height > grass2.height) {
+			material.color = grass2.color;
+		} else if (height > grass1.height) {
+			material.color = grass1.color;
+		} else if (height > sand3.height) {
+			material.color = sand3.color;
+		} else if (height > sand2.height) {
+			material.color = sand2.color;
+		} else if (height > sand1.height) {
+			material.color = sand1.color;
+		} else if (height > water3.height) {
+			material.color = water3.color;
+		} else if (height > water2.height) {
+			material.color = water2.color;
+		} else if (height > water1.height) {
+			material.color = water1.color;
 		}
 
 		const cube = new THREE.Mesh(geometry, material);
